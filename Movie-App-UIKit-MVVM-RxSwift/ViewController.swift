e716353df43aad3c2772a63748cdaaadb42ff0e1
david.aziz <david.aziz@evapharma.com>
2022-09-06 20:05:05 +0200

    Initial Commit

---
+++ Movie-App-UIKit-MVVM-RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-App-UIKit-MVVM-RxSwift
+//
+//  Created by David Aziz [Pharma] on 06/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
